3e00ef29a8901ffdca5aa4c1e25662b304a3673a
JBarbas <jbarbascubero@gmail.com>
2019-09-25 16:57:43 +0200

    El juego ahora es responsive

--- ZeroGravity/target/classes/static/src/index.js
+++ ZeroGravity/target/classes/static/src/index.js
@@ -2,15 +2,32 @@ window.onload = function() {
 	
 	config = {
 			  type: Phaser.AUTO,
-			  width: 800,
-			  height: 600,
+			  scale: {
+			        mode: Phaser.Scale.FIT,
+			        parent: 'phaser-example',
+			        autoCenter: Phaser.Scale.CENTER_BOTH,
+			        width: 1920,
+			        height: 1080			        
+			  },
 			  backgroundColor: "#222222",
-			  parent: "game-container",
 			  scene: [	
 				  		BootScene,
 				  		PreloadScene]
 			};
-
+	
+	/*config = {
+		    type: Phaser.AUTO,
+		    scale: {
+		        mode: Phaser.Scale.FIT,
+		        parent: 'phaser-example',
+		        autoCenter: Phaser.Scale.CENTER_BOTH,
+		        width: 800,
+		        height: 600
+		    },
+		    //... other settings
+		    scene: GameScene
+		};*/
+	
 	game = new Phaser.Game(config);
 	
 	// GLOBAL VARIABLES
