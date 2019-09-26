77ddc8d5a9c5d7f634fc1e2f2b5ccda265ef071b
JBarbas <jbarbascubero@gmail.com>
2019-09-26 20:05:58 +0200

    Construcción funcionando en cliente

--- ZeroGravity/target/classes/static/src/scenes/game.js
+++ ZeroGravity/target/classes/static/src/scenes/game.js
@@ -30,17 +30,30 @@ class GameScene extends Phaser.Scene {
     	// Establecemos los limites del mapa donde puede ver la camara
     	this.main_camera.setBounds(0-world_bounds_margin, 0-world_bounds_margin, tileMap_width*tile_width + 2*world_bounds_margin, tileMap_height*tile_height - 2*tile_height + 2*world_bounds_margin, true);
     	// Creamos la malla isometrica
-    	createGrid(this, tileMap_width, tileMap_height);
+    	this.mapGrid = createGrid(this, tileMap_width, tileMap_height);
+    	this.isDragging = false; // true si la cámara se está moviendo por drag del raton
+    	let scene = this;
+    	this.input.on('pointerup', function(pointer){
+    	    //var touchX = pointer.x;
+    	    //var touchY = pointer.y;
+    	    if (!scene.isDragging) {
+    	    	var position = new Phaser.Geom.Point(scene.main_camera.getWorldPoint(pointer.x, pointer.y).x - tileMap_width*tile_width/2, scene.main_camera.getWorldPoint(pointer.x, pointer.y).y);
+    	    	console.log(position);
+    			position = isometricToCartesian(position);
+    			let i = Math.trunc(position.y/tile_height + 1);
+    			let j = Math.trunc(position.x/(tile_width/2) + 1);
+    			console.log(i, j);
+    			let x = scene.mapGrid[i][j].image.x;
+    			let y = scene.mapGrid[i][j].image.y;
+    	    	var centroMando = new CentroMando(x, y);
+        	    mapGrid[i][j].contenido = scene.add.image(centroMando.x, centroMando.y, 'CentroMando').setOrigin(0.5, 1);
+			}
+			else {
+				scene.isDragging = false;
+			}
+    	 });
     }
     update(time, delta) {
-    	
-
-    		if(game.input.mousePointer.isDown){
-	    		var centroMando = new CentroMando(game.input.mousePointer.x, game.input.mousePointer.y);
-	    		console.log(centroMando.x)
-	    		this.add.image(centroMando.x, centroMando.y, 'CentroMando');
-    		}
-    	
 
     	////////////////////////////////////////////////////////////////////////////////////
     	// CONTROL DE CAMARA
@@ -52,20 +65,27 @@ class GameScene extends Phaser.Scene {
     	 * by sanojian - 14 October 2014
     	 */
     	if (this.game.input.activePointer.isDown) {
-		  if (this.game.origDragPoint) {
-			// move the camera by the amount the mouse has moved since last update
-			this.cameras.main.scrollX +=
-			  this.game.origDragPoint.x - this.game.input.activePointer.position.x;
-			this.cameras.main.scrollY +=
-			  this.game.origDragPoint.y - this.game.input.activePointer.position.y;
-		  } // set new drag origin to current position
-		  this.game.origDragPoint = this.game.input.activePointer.position.clone();
-		} else {
-		  this.game.origDragPoint = null;
+			  if (this.game.origDragPoint) {
+					// move the camera by the amount the mouse has moved since last update
+					this.cameras.main.scrollX += this.game.origDragPoint.x - this.game.input.activePointer.position.x;
+					this.cameras.main.scrollY += this.game.origDragPoint.y - this.game.input.activePointer.position.y;
+					if (this.game.origDragPoint.x - this.game.input.activePointer.position.x != 0 || this.game.origDragPoint.y - this.game.input.activePointer.position.y != 0) {
+						this.isDragging = true;
+					}
+			  } 
+			  // set new drag origin to current position
+			  this.game.origDragPoint = this.game.input.activePointer.position.clone();
+		} 
+    	else {
+			  this.game.origDragPoint = null;
 		}
     	/* Codigo extraido de http://www.html5gamedevs.com/topic/9814-move-camera-by-dragging-the-world-floor/
     	 * Updated by chrisme - 30 June 2019
     	 */
+    	
+    	this.input.on('pointerup', function(pointer){
+    	    this.isDragging = false;
+    	 });
     }
 
 }
@@ -78,13 +98,13 @@ function randomGrid(width, height) {
 		randomGrid.push(new Array());
 		for (var j = 0; j < width; j++) {
 			if (i >= minGridSide/3 + 1 && i < 2*minGridSide/3  + 1 && j >= minGridSide/3  + 1 && j < 2*minGridSide/3  + 1) {
-				randomGrid[i].push(0);
+				randomGrid[i].push({type: 0});
 			}
 			else if (i === 0 || i === height - 1 || j === 0 || j === width - 1) {
-				randomGrid[i].push(-2);
+				randomGrid[i].push({type: -2});
 			}
 			else {
-				randomGrid[i].push(-1);
+				randomGrid[i].push({type: -1});
 			}
 		}
 	}
@@ -97,10 +117,22 @@ function createGrid(scene, width, height) {
 	for (var i = 0; i < newGrid.length; i++) {
 		for (var j = 0; j < newGrid[i].length; j++) {
 			var position = new Phaser.Geom.Point(j*tile_width/2, i*tile_height);
+			//console.log(position);
 			position = cartesianToIsometric(position);
-			let tile = scene.add.image(tileMap_width*tile_width/2 + position.x, position.y, 'tile_prototipo_' + newGrid[i][j]).setOrigin(0.5, 1);
+			//console.log(position);
+			newGrid[i][j].image = scene.add.image(tileMap_width*tile_width/2 + position.x, position.y, 'tile_prototipo_' + newGrid[i][j].type).setOrigin(0.5, 1);
+			/*newGrid[i][j].image.setInteractive().on('pointerup', function(pointer, localX, localY, event){
+				if (!scene.isDragging) {
+					var centroMando = new CentroMando(this.x, this.y);
+		    		scene.add.image(centroMando.x, centroMando.y, 'CentroMando').setOrigin(0.5, 1);
+				}
+				else {
+					scene.isDragging = false;
+				}
+	    	});*/
 		}
 	}
+	return newGrid;
 }
 
 // Control del zoom
