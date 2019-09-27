6a2401f834a989d69926f7be08265fe43d759903
Mario <mariofdezfdez@hotmail.com>
2019-09-27 17:28:23 +0200

    Inicio instalacion Interfaces (III)

--- ZeroGravity/src/main/resources/static/src/scenes/boot.js
+++ ZeroGravity/src/main/resources/static/src/scenes/boot.js
@@ -14,10 +14,18 @@ class BootScene extends Phaser.Scene {
     
     preload () {
     	this.load.image('Lharys', 'assets/sprites/Lharys.png');
+    	this.load.image('background', 'assets/background/InicioSesionV.1.png');
+    	
+    	//Buttons
+    	this.load.image('session', 'assets/background/btnInicioSesion.png');
     }
     create (data)  {
-    	var image = this.add.image(400, 300, 'Lharys');
-    	image.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
+    	var background = this.add.image(960, 540, 'background');
+    	
+    	var button = this.add.image(960, 800, 'session');
+    	/*var buttonHover = this.add.image(960, 800, 'sessionHover');*/
+        
+    	button.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
     		game.scene.run('PreloadScene');
     		game.scene.stop('BootScene');
     	});
--- ZeroGravity/target/classes/static/src/scenes/preload.js
+++ ZeroGravity/target/classes/static/src/scenes/preload.js
@@ -20,10 +20,15 @@ class PreloadScene extends Phaser.Scene {
 		this.load.image('tile_prototipo_-1', 'assets/sprites/Tiles_prototipo/tile_prototipo_-1.png');
 		this.load.image('tile_prototipo_-2', 'assets/sprites/Tiles_prototipo/tile_prototipo_-2.png');
 
+		this.load.image('backgroundM', 'assets/background/MenuPrincipal.png');
+		this.load.image('btnAmigos', 'assets/background/btnMenuAmigos.png');
     }
     create (data)  {
-    	var image = this.add.image(300, 300, 'Lharys');
-    	image.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
+    	var backgroundM = this.add.image(960, 540, 'backgroundM');
+    	
+    	var btnAmigos = this.add.image(1720, 950, 'btnAmigos');
+    	btnAmigos.setScale(1.5);
+    	btnAmigos.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
     		game.scene.run('GameScene');
     		game.scene.stop('PreloadScene');
     	});
