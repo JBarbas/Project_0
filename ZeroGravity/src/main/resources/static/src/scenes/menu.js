6b43deda6f93431aa0be0808cb2397873be33f0a
JBarbas <jbarbascubero@gmail.com>
2019-09-28 20:08:11 +0200

    oootra vez estructurando

--- ZeroGravity/src/main/resources/static/src/scenes/menu.js
+++ ZeroGravity/src/main/resources/static/src/scenes/menu.js
@@ -24,6 +24,63 @@ class MenuScene extends Phaser.Scene {
     		game.scene.run('PreloadGameScene');
     		game.scene.stop('MenuScene');
     	});
+    	
+    	//var textT = this.add.bitmapText(200, 50, 'myfont', 'Holaaaaaaaaaa', 128); 
+    	
+    	
+    	//TODO  HAY QUE ARREGLAR ESTO PORQUE NO FUNCIONA VENGA CHAO
+    	var title = this.add.sprite(200, 200, 'title', 'background.png');
+    	
+    	var jugar = this.add.text(80, 400, 'JUGAR', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
+    	var opciones = this.add.text(80, 500, 'OPCIONES', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
+    	var creditos = this.add.text(80, 600, 'CRÉDITOS', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
+
+    	var size = '50px';
+    	
+    	var colorYellow = '#ffd213';
+    	var colorWhite = '#fff';
+    	
+    	jugar.setFontSize(size);
+    	opciones.setFontSize(size);
+    	creditos.setFontSize(size);
+    	
+    	
+    	
+    	jugar.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
+    		game.scene.run('PreloadGameScene');
+    		game.scene.stop('MenuScene');
+    	});
+    	jugar.setInteractive().on('pointerover', function(pointer, localX, localY, event){
+    		jugar.setFill(colorYellow);
+    	});
+    	jugar.setInteractive().on('pointerout', function(pointer, localX, localY, event){
+    		jugar.setFill(colorWhite);
+    	});
+    	
+    	opciones.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
+    		game.scene.run('PreloadGameScene');
+    		game.scene.stop('MenuScene');
+    	});
+    	opciones.setInteractive().on('pointerover', function(pointer, localX, localY, event){
+    		opciones.setFill(colorYellow);
+    	});
+    	opciones.setInteractive().on('pointerout', function(pointer, localX, localY, event){
+    		opciones.setFill(colorWhite);
+    	});
+    	
+    	creditos.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
+    		game.scene.run('PreloadGameScene');
+    		game.scene.stop('MenuScene');
+    	});
+    	creditos.setInteractive().on('pointerover', function(pointer, localX, localY, event){
+    		creditos.setFill(colorYellow);
+    	});
+    	creditos.setInteractive().on('pointerout', function(pointer, localX, localY, event){
+    		creditos.setFill(colorWhite);
+    	});
+    	
+    	
+
     }
     update(time, delta) {
     	
--- ZeroGravity/src/main/resources/static/src/scenes/preload.js
+++ ZeroGravity/src/main/resources/static/src/scenes/preload.js
@@ -27,75 +27,8 @@ class PreloadScene extends Phaser.Scene {
 		
     }
     create (data)  {
-
-    	
-    	
-    	
-    	//var textT = this.add.bitmapText(200, 50, 'myfont', 'Holaaaaaaaaaa', 128); 
-    	
-    	var backgroundM = this.add.image(960, 540, 'backgroundM');
-    	
-    	
-    	//TODO  HAY QUE ARREGLAR ESTO PORQUE NO FUNCIONA VENGA CHAO
-    	var title = this.add.sprite(200, 200, 'title', 'background.png');
-    	
-    	
-    	var btnAmigos = this.add.image(1720, 950, 'btnAmigos');
-    	btnAmigos.setScale(1.5);
-    	
-    	var jugar = this.add.text(80, 400, 'JUGAR', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
-    	var opciones = this.add.text(80, 500, 'OPCIONES', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
-    	var creditos = this.add.text(80, 600, 'CRÉDITOS', { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif' });
-
-    	var size = '50px';
-    	
-    	var colorYellow = '#ffd213';
-    	var colorWhite = '#fff';
-    	
-    	jugar.setFontSize(size);
-    	opciones.setFontSize(size);
-    	creditos.setFontSize(size);
-    	
-    	
-    	
-    	jugar.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
-    		game.scene.run('GameScene');
-    		game.scene.stop('PreloadScene');
-    	});
-    	jugar.setInteractive().on('pointerover', function(pointer, localX, localY, event){
-    		jugar.setFill(colorYellow);
-    	});
-    	jugar.setInteractive().on('pointerout', function(pointer, localX, localY, event){
-    		jugar.setFill(colorWhite);
-    	});
-    	
-    	opciones.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
-    		game.scene.run('GameScene');
-    		game.scene.stop('PreloadScene');
-    	});
-    	opciones.setInteractive().on('pointerover', function(pointer, localX, localY, event){
-    		opciones.setFill(colorYellow);
-    	});
-    	opciones.setInteractive().on('pointerout', function(pointer, localX, localY, event){
-    		opciones.setFill(colorWhite);
-    	});
-    	
-    	creditos.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
-    		game.scene.run('GameScene');
-    		game.scene.stop('PreloadScene');
-    	});
-    	creditos.setInteractive().on('pointerover', function(pointer, localX, localY, event){
-    		creditos.setFill(colorYellow);
-    	});
-    	creditos.setInteractive().on('pointerout', function(pointer, localX, localY, event){
-    		creditos.setFill(colorWhite);
-    	});
-    	
-    	
-
 		game.scene.run('LogInScene');
 		game.scene.stop('PreloadScene');
-
     }
     update(time, delta) {
     	
