8026ab38974a88ae790dd6acc79f147ade3b0d55
Mario <mariofdezfdez@hotmail.com>
2019-09-30 18:58:18 +0200

    subida de iconos

--- ZeroGravity/src/main/resources/static/src/scenes/loadGame.js
+++ ZeroGravity/src/main/resources/static/src/scenes/loadGame.js
@@ -19,18 +19,12 @@ class LoadGameScene extends Phaser.Scene {
     	var background = this.add.image(960, 540, 'backgroundLoadGame');
     	
     	
-    	this.add.tween(background).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None,0, 1000, true);
+    	//this.add.tween(background).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None,0, 1000, true);
     	
-    	/*setTimeout(function(){
-    		this.tweens.add({
-    	          targets: background,
-    	          alpha: 0,
-    	          duration: 300,
-    	          ease: 'Power2'
-    	        }, this);
+    	setTimeout(function(){
     		game.scene.run('LogInScene');
     		game.scene.stop('LoadGameScene');
-    	}, 3000);*/
+    	}, 3000);
     	/*var time = game.time.events.add(Phaser.Timer.SECOND * 4, fadePicture, this);
     	
     	
