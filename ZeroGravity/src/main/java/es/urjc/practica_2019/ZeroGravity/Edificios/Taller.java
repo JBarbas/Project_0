33b78dff5b528b24166867c7529125361eccc1fc
Voidtekvla <43203570+Voidtekvla@users.noreply.github.com>
2019-10-20 21:56:16 +0200

    costes nivel

--- ZeroGravity/src/main/java/es/urjc/practica_2019/ZeroGravity/Edificios/Taller.java
+++ ZeroGravity/src/main/java/es/urjc/practica_2019/ZeroGravity/Edificios/Taller.java
@@ -25,9 +25,9 @@ public class Taller extends GeneradorRecursos{
 	private static final TaskMaster TASKMASTER = TaskMaster.INSTANCE;
 
 	// Establecemos los costes por cada nivel: Energia, Metal, Ceramica, Creditos
-	public static final int[] NIVEL1 = { 1, 0, 0, 0, 5};
-	public static final int[] NIVEL2 = { 2, 0, 0, 0, 5};
-	public static final int[] NIVEL3 = { 3, 0, 0, 0, 5};
+	public static final int[] NIVEL1 = { 1, 12, 20, 400, 5};
+	public static final int[] NIVEL2 = { 3, 180, 250, 6000, 5};
+	public static final int[] NIVEL3 = { 5, 1100, 2000, 40000, 5};
 	public static final int[][] COSTS = { NIVEL1, NIVEL2, NIVEL3};
 	
 	//Establecemos los recursos que generan según su nivel
