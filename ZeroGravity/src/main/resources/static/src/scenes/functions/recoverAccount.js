e3ebf06221013175b78d3227d91a3ff34438ed16
lmml-l <43039968+lmml-l@users.noreply.github.com>
2020-04-16 11:41:09 +0200

    Sweet Alert en RecoverPassword y no mas reload de pagina
    -Se ha solucionado el problema que al hacer submit se recargaba la pagina de nuevo
    -Se ha añadido una sweetalert provisional

--- ZeroGravity/src/main/resources/static/src/scenes/functions/recoverAccount.js
+++ ZeroGravity/src/main/resources/static/src/scenes/functions/recoverAccount.js
@@ -1,38 +1,43 @@
 var recoverAccountPage = {
-	socket:null,
-	recoverfunction:null
+	socket:null
 };
 
-
-window.onload = function() {
-	recoverAccountPage.socket = new WebSocket("ws://" + location.href.substring(7).split("/")[0] + "/polaris");
+recoverAccountPage.socket = new WebSocket("ws://" + location.href.substring(7).split("/")[0] + "/polaris");
 	
 	
-	recoverAccountPage.socket.onopen = () => {
+recoverAccountPage.socket.onopen = () => {
 		console.log('[DEBUG] WebSocket connection opened on RecoverPasswordSite')
-	}
-	
-	recoverAccountPage.socket.onclose = () => {
-			console.log('[DEBUG] WebSocket connection closed on RecoverPasswordSite')
-	}
-	
-	recoverAccountPage.socket.onmessage = (message) => {
-		var msg = JSON.parse(message.data)
-		
-		switch(msg.event){
-			case 'RECOVERED PASSWORD':
-				break;
-			default:
-		}
-	}
-
-	var docButt = document.getElementById('Summit');
-	var docEmailField = document.getElementById('EmailField');
-
-	docButt.addEventListener('click',function(pointer, localX, localY, event){
-		let msg = new Object();
-        msg.event = 'RECOVER PASSWORD';
-        msg.email = docEmailField.value;
-		recoverAccountPage.socket.send(JSON.stringify(msg));
-	});
 }
+	
+recoverAccountPage.socket.onclose = () => {
+		console.log('[DEBUG] WebSocket connection closed on RecoverPasswordSite')
+}
+	
+recoverAccountPage.socket.onmessage = (message) => {
+	var msg = JSON.parse(message.data)
+		
+	switch(msg.event){
+		case 'RECOVERED PASSWORD':
+			break;
+		default:
+	}
+}
+
+var docButt = document.getElementById('Submit');
+var docEmailField = document.getElementById('EmailField');
+
+docButt.addEventListener('click',function(){
+	let msg = new Object();
+    msg.event = 'RECOVER PASSWORD';
+    msg.email = docEmailField.value;
+	recoverAccountPage.socket.send(JSON.stringify(msg));
+
+	swal("Good job!", "If the email is correct you will receive one email soon", "success")
+	return false;
+	
+});
+
+//Esto es para que al dar sumit no se recargue la pagina de nuevo
+var form = document.getElementById("myForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
