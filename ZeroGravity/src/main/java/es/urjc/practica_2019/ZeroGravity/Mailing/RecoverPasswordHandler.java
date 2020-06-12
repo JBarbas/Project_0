add0bb302b97bdc5d4a7f87d8ebafa9d5935cd94
Mario <mariofdezfdez@hotmail.com>
2020-06-12 15:16:11 +0200

    validate account

--- ZeroGravity/src/main/java/es/urjc/practica_2019/ZeroGravity/Mailing/RecoverPasswordHandler.java
+++ ZeroGravity/src/main/java/es/urjc/practica_2019/ZeroGravity/Mailing/RecoverPasswordHandler.java
@@ -59,7 +59,7 @@ public class RecoverPasswordHandler extends MailingHandler{
 		emailService.prepareAndSend(to,from, subject,content,fixedResourcesEmbebed);
 	}
 	
-	private String generatePassword() {
+	public static String generatePassword() {
 		String password = null;
 		try {
 		String[] symbols = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
--- ZeroGravity/src/main/java/es/urjc/practica_2019/ZeroGravity/Mailing/validateEmailHandler.java
+++ ZeroGravity/src/main/java/es/urjc/practica_2019/ZeroGravity/Mailing/validateEmailHandler.java
@@ -1,5 +1,14 @@
 package es.urjc.practica_2019.ZeroGravity.Mailing;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -18,6 +27,7 @@ import com.mongodb.client.MongoCollection;
 
 import es.urjc.practica_2019.ZeroGravity.MailContentBuilder;
 import es.urjc.practica_2019.ZeroGravity.WebsocketGameHandler;
+import es.urjc.practica_2019.ZeroGravity.Mailing.RecoverPasswordHandler;
 
 
 @Controller
@@ -34,12 +44,41 @@ public class validateEmailHandler extends MailingHandler{
 		executor.execute(()->validateEmailHandler(to));
 	}
 	
-	private void validateEmailHandler(String to) {
-	
+	private void validateEmailHandler(String to){
+		System.out.println("Entra o que");
 		//Añadimos la contraseña al mensaje y lo mandamos
 		HashMap<String,String> auxHashmap =  new HashMap<String, String>(fixedContentAtributes);
 		
+		String code = RecoverPasswordHandler.generatePassword();
 		String content = MailContentBuilder.build(auxHashmap,layout);
 		emailService.prepareAndSend(to,from, subject,content,fixedResourcesEmbebed);
+		
+		//Crear fichero
+		//File original = new File("src/test/resources/copiedWithIo.txt");
+		File copied = new File("src/main/resources/static/validate/" + code + ".html");
+	    try (
+	      InputStream in = new BufferedInputStream(
+	        new FileInputStream("src/main/resources/static/assets/text/validateMenu.html"));
+	      OutputStream out = new BufferedOutputStream(
+	        new FileOutputStream(copied))) {
+	  
+	        byte[] buffer = new byte[1024];
+	        int lengthRead;
+	        while ((lengthRead = in.read(buffer)) > 0) {
+	            out.write(buffer, 0, lengthRead);
+	            out.flush();
+	        }
+	    }catch(FileNotFoundException e) {
+	    	System.out.println("Que passsaa");
+	    	e.printStackTrace();
+	    }
+	    catch(IOException e) {
+	    	System.out.println("Barbitas");
+	    	e.printStackTrace();
+	    }
+	  
+	    /*assertThat(copied).exists();
+	    assertThat(Files.readAllLines(original.toPath())
+	      .equals(Files.readAllLines(copied.toPath())));*/
 	}
 }
